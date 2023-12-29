32d7a9be0770631d8ccafd965de9f1ee36f50dea
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2023-12-29 09:33:47 +0300

    Create components and implement functionalites.

---
+++ src/components/HeroSection/hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './hero.css';
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <h1>Front-end developement test project</h1>
+      <p>My goal was to make a page that looks exactly like this one, 
+        and has the ability to create H1 text simply by typing / then 1, then typing text and hitting enter.</p>
+    </div>
+  );
+};
+
+export default HeroSection;
